cb136adf5e352e2a68058a91565fdf45d19c20b1
Owen McGrath <mail@owm111.me>
2022-05-16 17:05:16 -0500

    Update dependencies

--- shell.nix
+++ shell.nix
@@ -6,6 +6,7 @@ mkShell {
   buildInputs = [ (haskellPackages.ghcWithPackages (h: with h; [
     # libraries
     xmonad xmonad-contrib
+    data-default X11
     # tools
     cabal-install
   ])) ];
