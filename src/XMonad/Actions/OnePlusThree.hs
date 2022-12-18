c8683993a97efcc2bc21ea790f57764bb65f5d98
Owen McGrath <mail@owm111.me>
2022-12-18 10:14:24 -0600

    Add XMonad.Actions.OnePlusThree

---
+++ src/XMonad/Actions/OnePlusThree.hs
@@ -0,0 +1,72 @@
+{-|
+This module provides actions for quickly switching workspaces on all
+screens, keeping one "fixed" while the others change to a set of three
+workspaces. It is intended for use on a 1+3 setup, with one monitor
+above a row of three.
+
+Similar to "XMonad.Actions.Workscreen", but this module allows keeping
+a workscreen fixed to a screen and does not yet allow customization of
+which workspaces are apart of each set.
+
+Possible TODO:
+
+- Generalize to any monitor setup
+- Different ways to select which workspaces are part of each set
+- Deal with dynamic workspaces
+-}
+
+{-# LANGUAGE NamedFieldPuns #-}
+{-# LANGUAGE ImportQualifiedPost #-}
+
+module XMonad.Actions.OnePlusThree
+    ( viewWorkspaceSet
+    , workspaceSetKeys
+    ) where
+
+import Control.Monad.Trans.Class
+import Control.Monad.Trans.Maybe
+import Data.Map.Strict qualified as M
+import XMonad
+import XMonad.Actions.OnScreen
+import XMonad.Actions.PhysicalScreens
+import XMonad.StackSet qualified as W
+
+{-|
+View the given workspace set. Workspace set /n/ is defined as workspace 0
+on screen 0, workspace 3/n/+1 on screen 1, workspace 3/n/+2 on screen 2,
+and workspace 3/n/+3 on screen 3.
+-}
+viewWorkspaceSet ::
+    {-| Value of /n/. -}
+    Int ->
+    X ()
+viewWorkspaceSet = fmap (const ()) . runMaybeT . viewWorkspaceSet'
+
+viewWorkspaceSet' n = do
+    screens <- traverse (MaybeT . getScreen def) [0 .. 3]
+    wksp0 : wksps <- asks (workspaces . config)
+    otherWorkspaces@[_, middle, _] <- pure . take 3 . drop (3 * n) $ wksps
+    mapM_ goPair (zip screens (wksp0 : otherWorkspaces))
+    lift (windows (W.view middle))
+
+goPair (screen, wksp) = lift (windows (greedyViewOnScreen screen wksp))
+
+{-|
+Generate a keybind map of 'viewWorkspaceSet' for all workspaces with
+the same mask and a given set of symbols. The result can be combined
+with other maps monoidally, i.e., via '<>'.
+-}
+workspaceSetKeys ::
+    {-| The mask to use for all the binds of this map. The argument is
+    the 'XConfig'\'s 'modMask'. For just the 'modMask', use @id@. For
+    'modMask' with another mask, use @(.|. mask)@. For something that
+    does involve the 'modMask', use @(const mask)@. -}
+    (KeyMask -> KeyMask) ->
+    {-| List of key symbols to use for mappings. To use the number keys,
+    use @[xK_1 ..]@. -}
+    [KeySym] ->
+    {-| Resulting map of keybinds. -}
+    (XConfig Layout -> M.Map (KeyMask, KeySym) (X ()))
+workspaceSetKeys maskf keys XConfig {modMask, workspaces} = M.fromList $ do
+    (i, key) <- zip [0 .. (length workspaces - 1) `div` 3] keys
+    pure ((maskf modMask, key), viewWorkspaceSet i)
